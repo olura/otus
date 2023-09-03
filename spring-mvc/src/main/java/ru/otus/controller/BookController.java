99183261da826efe92c68bfa2fa36154e8e036a2
olura <foodmen243@yandex.ru>
2023-09-03 11:20:11 +0300

    Add book pages

--- spring-mvc/src/main/java/ru/otus/controller/BookController.java
+++ spring-mvc/src/main/java/ru/otus/controller/BookController.java
@@ -1,16 +1,17 @@
 package ru.otus.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.otus.domain.Book;
+import ru.otus.exception.BookNotFoundException;
 import ru.otus.service.BookService;
 
 import java.util.List;
 
 @Controller
-//@RequiredArgsConstructor
 public class BookController {
 
     private final BookService bookService;
@@ -20,9 +21,31 @@ public class BookController {
     }
 
     @GetMapping ("/")
-    public String viewBook(Model model) {
+    public String viewBooks(Model model) {
         List<Book> bookList = bookService.getAll();
         model.addAttribute("books", bookList);
-        return "viewBook";
+        return "viewBooks";
+    }
+
+    @GetMapping ("/book/{id}")
+    public String viewBook(Model model, @PathVariable long id) {
+        try {
+            Book book = bookService.getById(id);
+            model.addAttribute("book", book);
+            return "viewBook";
+        } catch (BookNotFoundException e) {
+            return "bookNotFound";
+        }
+    }
+
+    @GetMapping ("/edit")
+    public String editBook(Model model, @RequestParam("id") long id) {
+        try {
+            Book book = bookService.getById(id);
+            model.addAttribute("book", book);
+            return "edit";
+        } catch (BookNotFoundException e) {
+            return "bookNotFound";
+        }
     }
 }
